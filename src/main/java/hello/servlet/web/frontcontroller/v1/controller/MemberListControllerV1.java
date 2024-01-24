6b464f63084c75baecaf37f1166ea3a9e00f66bb
teawaem <monkey587@naver.com>
2024-01-24 15:35:20 +0900

    프론트 컨트롤러

--- src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
+++ src/main/java/hello/servlet/web/frontcontroller/v1/controller/MemberListControllerV1.java
@@ -1,24 +1,23 @@
-package hello.servlet.web.servletmvc;
+package hello.servlet.web.frontcontroller.v1.controller;
 
 import hello.servlet.domain.member.Member;
 import hello.servlet.domain.member.MemberRepository;
+import hello.servlet.web.frontcontroller.v1.ControllerV1;
 import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "mvcMemberListServlet", urlPatterns = "/servlet-mvc/members")
-public class MvcMemberListServlet extends HttpServlet {
+public class MemberListControllerV1 implements ControllerV1 {
+
 
     private MemberRepository memberRepository = MemberRepository.getInstance();
 
     @Override
-    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    public void Process(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         List<Member> members = memberRepository.findALl();
 
@@ -27,5 +26,6 @@ public class MvcMemberListServlet extends HttpServlet {
         String viewPath = "/WEB-INF/views/members.jsp";
         RequestDispatcher dispatcher = request.getRequestDispatcher(viewPath);
         dispatcher.forward(request, response);
+
     }
 }
