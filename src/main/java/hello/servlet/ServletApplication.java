995b9c7f212bf14b341cd9017785e236d13c4f43
teawaem <monkey587@naver.com>
2024-02-01 21:02:15 +0900

    MVC 구조

--- src/main/java/hello/servlet/ServletApplication.java
+++ src/main/java/hello/servlet/ServletApplication.java
@@ -3,13 +3,20 @@ package hello.servlet;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @ServletComponentScan 	// 서블릿 자동 등록
 @SpringBootApplication
 public class ServletApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ServletApplication.class, args);
 	}
-
+//	스프링부트가 다 해주기 때문에 코드를 작성하지 않아도 된다.
+//	@Bean
+//	ViewResolver internalResourceViewResolver(){
+//		return new InternalResourceViewResolver("/WEB-INF/vies/", ".jsp");
+//	}
 }
+
---
+++ src/main/java/hello/servlet/web/springmvc/v1/SpringMemberListControllerV1.java
@@ -0,0 +1,29 @@
+package hello.servlet.web.springmvc.v1;
+
+import hello.servlet.domain.member.Member;
+import hello.servlet.domain.member.MemberRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+public class SpringMemberListControllerV1 {
+
+    MemberRepository memberRepository = MemberRepository.getInstance();
+
+    @RequestMapping("/springmvc/v1/members")
+    public ModelAndView process() {
+
+        List<Member> members = memberRepository.findALl();
+
+        ModelAndView mv = new ModelAndView("members");
+        mv.addObject("members",members);
+
+        return mv;
+
+    }
+}
